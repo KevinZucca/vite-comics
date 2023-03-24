480cbe958ecf4b84fadbc5febcd8b279fd16be39
KevinZucca <kev.94@live.it>
2023-03-24 16:39:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  import AppHeader from "./components/AppHeader.vue"
+
+
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    components: {
+      AppHeader,
+    },
+
+    methods: {}
+
+  }
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style>
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,61 @@
+<script>
+  export default {
+    data () {
+      return {
+        links: [
+            "characters",
+            "comics",
+            "movies",
+            "tv",
+            "games",
+            "collectibles",
+            "videos",
+            "fans",
+            "news",
+            "shop"
+        ]
+      };
+    },
+    
+  }
+</script>
+
+
+<template>
+    <header>
+        <div class="container-centered">
+            <div id="logo">
+                <img src="/public/img/dc-logo.png" alt="logo">
+            </div>
+            <div id="links" v-for="link in links">
+                {{ link }}
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+    header {
+        height: 120px;
+        border: 1px solid black;
+       
+
+        .container-centered {
+            height: 100%;
+            padding: 10px;
+
+            #logo{
+                flex-grow: 1;
+            }
+
+
+            #links {
+                text-transform: uppercase;
+                font-weight: bold;
+                color: #464646;
+                padding: 8px;
+            }
+        }
+    }
+</style>
